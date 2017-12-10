8cb756825614f640fa5a4bc11fcc69c104577ee9
MilonLemon <maksudyan_mila@mail.ru>
2017-12-10 03:20:03 +0300

    Update app-organizer.js

--- app-organizer.js
+++ app-organizer.js
@@ -1,75 +1,68 @@
 window.addEventListener('load', function () {
 
-    var webAuth = new auth0.WebAuth({
-        domain: 'organizedthoughts.auth0.com',
-        clientID: 'fY6gtnOhfgomjVsasAL90rKRoNx8a2j0',
-        responseType: 'token id_token',
-        audience: 'https://organizedthoughts.auth0.com/userinfo',
-        scope: 'openid',
-        redirectUri: 'https://organizedthoughts.auth0.com/v2/logout',
-        leeway: 60
-    });
+var webAuth = new auth0.WebAuth({
+  domain: 'organizedthoughts.auth0.com',
+  clientID: 'fY6gtnOhfgomjVsasAL90rKRoNx8a2j0',
+  responseType: 'token id_token',
+  audience: 'https://organizedthoughts.auth0.com/userinfo',
+  scope: 'openid',
+  redirectUri: 'https://organizedthoughts.auth0.com/v2/logout',
+  leeway: 60
+});
 
-    <script src="https://cdn.auth0.com/js/auth0/8.10.1/auth0.min.js"></script>
+var name = document.getElementById('name');
+var logoutBtn = document.getElementById('btn-logout');
 
-    var name = document.getElementById('name');
-    var logoutBtn = document.getElementById('btn-logout');
+logoutBtn.addEventListener('click', logout);
 
-    logoutBtn.addEventListener('click', logout);
+function setSession(authResult) {
+  // Set the time that the access token will expire at
+  var expiresAt = JSON.stringify(
+    authResult.expiresIn * 1000 + new Date().getTime()
+  );
+  localStorage.setItem('access_token', authResult.accessToken);
+  localStorage.setItem('id_token', authResult.idToken);
+  localStorage.setItem('expires_at', expiresAt);
+  displayButtons();
+}
 
-    function setSession(authResult) {
-        // Set the time that the access token will expire at
-        var expiresAt = JSON.stringify(
-          authResult.expiresIn * 1000 + new Date().getTime()
-        );
-        localStorage.setItem('access_token', authResult.accessToken);
-        localStorage.setItem('id_token', authResult.idToken);
-        localStorage.setItem('expires_at', expiresAt);
-        displayButtons();
-      }
-    
-      function logout() {
-        // Remove tokens and expiry time from localStorage
-        localStorage.removeItem('access_token');
-        localStorage.removeItem('id_token');
-        localStorage.removeItem('expires_at');
-      }
-    
-      function isAuthenticated() {
-        // Check whether the current time is past the
-        // access token's expiry time
-        var expiresAt = JSON.parse(localStorage.getItem('expires_at'));
-        return new Date().getTime() < expiresAt;
-      }
-      
-      webAuth.logout({
-        returnTo: 'https://milonlemon.github.io',
-        client_id: 'fY6gtnOhfgomjVsasAL90rKRoNx8a2j0'
-      });
+function logout() {
+  // Remove tokens and expiry time from localStorage
+  localStorage.removeItem('access_token');
+  localStorage.removeItem('id_token');
+  localStorage.removeItem('expires_at');
+}
 
-      function handleAuthentication() {
-        webAuth.parseHash(function(err, authResult) {
-          if (authResult && authResult.accessToken && authResult.idToken) {
-            window.location.hash = '';
-            setSession(authResult);
-          } else if (err) {
-            console.log(err);
-            alert(
-              'Error: ' + err.error + '. Check the console for further details.'
-            );
-          }
-          displayButtons();
-        });
-      }
-    
-      function displayButtons() {
-        if (isAuthenticated()) {
-          name.textContent = 'You are logged in!';
-        } else {
-          name.textContent = 'You are not logged in!';
-          //location.href='"https://milonlemon.github.io';
-        }
-      }
-    
-      handleAuthentication();
+function isAuthenticated() {
+  // Check whether the current time is past the
+  // access token's expiry time
+  var expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+  return new Date().getTime() < expiresAt;
+}
+
+function handleAuthentication() {
+  webAuth.parseHash(function(err, authResult) {
+    if (authResult && authResult.accessToken && authResult.idToken) {
+      window.location.hash = '';
+      setSession(authResult);
+    } else if (err) {
+      console.log(err);
+      alert(
+        'Error: ' + err.error + '. Check the console for further details.'
+      );
+    }
+    displayButtons();
+  });
+}
+
+function displayButtons() {
+  if (isAuthenticated()) {
+    name.textContent = 'You are logged in!';
+  } else {
+    name.textContent = 'You are not logged in!';
+    //location.href='"https://milonlemon.github.io';
+  }
+}
+
+handleAuthentication();
 });
